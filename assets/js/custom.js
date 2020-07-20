4dab7fc6a467f885fd773c81efc9e2e6a0c85dc1
Christina Lozovaya <k7772210@gmail.com>
2020-07-20 18:01:10 +0300

    add sections with diagonal slider and preloader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function(){
+    // /* preloader */
+    // setTimeout(function() {
+    //     $('body').addClass('loaded');
+    //     $('h1').css('color', '#222222')
+    // }, 2000);
+    // /* /preloader */
+    $('body').addClass('loaded');
+
+
+//my diagonal slider
+    $('.diagonal-box').on('click', diagonalItemActive);
+    $('.diagonal__wrapper').on('click', diagonalItemDeactivate);
+
+    function diagonalItemActive(e) {
+        e.stopPropagation();
+        $(this).parent().addClass('active').children().removeClass('active');
+        $(this).addClass('active');
+    }
+
+    function diagonalItemDeactivate() {
+        if( $(this) !== $(this).children() ) {
+            $(this).removeClass('active').children().removeClass('active');
+        }
+    }
+
+//diagonal slider by nnvenio
+    $(document).ready(function(){
+        $('.gallery_content').createDiagonalSlider();
+    });
 
 });
